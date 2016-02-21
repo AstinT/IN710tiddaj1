1159987dcc5bed0821d922e898e91f59b9056769
Astin Tiddy <Astin8@hotmail.co.nz>
2016-02-21 18:58:45 +1300

    Worked on bridge

---
+++ Weekly practicals/Week 1/Dealer 2/WindowsFormsApplication1/THand.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsFormsApplication1
+{
+    class THand
+    {
+        private TCard[] cards;
+        private int privatePoints;
+        private int numCards;
+
+        public int points
+        {
+            get
+            {
+                return privatePoints;
+            }
+
+            set
+            {
+                privatePoints = value;
+            }
+        }
+
+        public THand()
+        {
+            cards = new TCard[13];
+        }
+
+        public THand(TCard[] cardArray)
+        {
+            cards = new TCard[13];
+
+            for (int i = 0; i < 13; i++)
+            {
+                cards[i] = cardArray[i];
+            }
+        }
+
+        public void addCard(TCard newCard)
+        {
+            cards[numCards++] = newCard;
+        }
+
+        public int calcPoints()
+        {
+            privatePoints = 0;
+            for (int i = 0; i < cards.Length; i++)
+            {
+                switch (cards[i].number)
+                {
+                    case 14:
+                        privatePoints += 4;
+                        break;
+
+                    case 11:
+                        privatePoints += 1;
+                        break;
+
+                    case 12:
+                        privatePoints += 2;
+                        break;
+
+                    case 13:
+                        privatePoints += 3;
+                        break;
+                }
+            }
+
+            return privatePoints;
+        }
+
+        public void sortHand()
+        {
+            for (int i = 0; i < cards.Length; i++)
+            {
+                TCard minCard = cards[i];
+                int minPos = i;
+                for (int j = i; j < cards.Length; j++)
+                {
+                    if (cards[j].suit < minCard.suit)
+                    {
+                        minCard = cards[j];
+                        minPos = j;
+                    }
+                    else if (cards[j].suit == minCard.suit)
+                    {
+                        if (cards[j].number < minCard.number)
+                        {
+                            minCard = cards[j];
+                            minPos = j;
+                        }
+                    }
+                }
+                TCard tempCard = cards[minPos];
+                cards[minPos] = cards[i];
+                cards[i] = tempCard;
+            }
+        }
+
+        public override string ToString()
+        {
+            String spades = "S:\t";
+            String hearts = "H:\t";
+            String diamonds = "D:\t";
+            String clubs = "C:\t";
+            String myHand = "";
+            
+            for (int i = 0; i < 13; i++)
+            {
+                int suit = cards[i].suit;
+                int number = cards[i].number;
+
+                
+            }
+
+            myHand = spades + "\n" + hearts + "\n" + diamonds + "\n" + clubs + "\n";
+
+            return myHand + "\r\n";
+        }
+
+    }
+}
