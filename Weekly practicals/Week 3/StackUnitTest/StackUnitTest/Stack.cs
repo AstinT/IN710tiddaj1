c325bfd801c0718661680afcc5b86f0cfc0c48e9
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-06 19:24:45 +1300

    Initial commit
    Created project. Added stack class, method stubs and unit test project.

---
+++ Weekly practicals/Week 3/StackUnitTest/StackUnitTest/Stack.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUnitTest
+{
+    public class Stack
+    {
+        public Stack()
+        {
+           
+        }
+
+        public void Push()
+        {
+            
+        }
+
+        public String Pop()
+        {
+            
+        }
+
+        public String Peek()
+        {
+
+        }
+
+        public int Count()
+        {
+            
+        }
+
+        public bool isEmpty()
+        {
+                 
+        }
+    }
+}
