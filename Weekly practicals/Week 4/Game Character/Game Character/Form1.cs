0a724ebd7514c5f4857c45a6b60aea246bbe7b79
AstinT <Astin8@hotmail.co.nz>
2016-03-10 19:02:23 +1300

    Added methods
    Added UpdateCheckedListBox and btnAddCharacter_Click

--- Weekly practicals/Week 4/Game Character/Game Character/Form1.cs
+++ Weekly practicals/Week 4/Game Character/Game Character/Form1.cs
@@ -24,6 +24,15 @@ namespace Game_Character
             characterList.Add(new Knight("Bjorn"));
             characterList.Add(new Troll("Floki"));
 
+            UpdateCheckedListBox();
+        }
+
+        public void UpdateCheckedListBox()
+        {
+            //Clearing checkedListBoxes
+            checkedListBox1.Items.Clear();
+            checkedListBox2.Items.Clear();
+
             //Adding characters to checkedListBox
             foreach (Character character in characterList)
             {
@@ -34,7 +43,32 @@ namespace Game_Character
 
         private void btnAddCharacter_Click(object sender, EventArgs e)
         {
+            Character newCharacter = null;
+            String name = txtName.Text;
 
+            if (txtName.Text != name)
+            {
+                if (rdKing.Checked)
+                    newCharacter = new King(name);
+                else if (rdQueen.Checked)
+                    newCharacter = new Queen(name);
+                else if (rdKnight.Checked)
+                    newCharacter = new Knight(name);
+                else if (rdTroll.Checked)
+                    newCharacter = new Troll(name);
+                else
+                    MessageBox.Show("Please select a character type");
+
+                if (newCharacter != null)
+                {
+                    characterList.Add(newCharacter);
+                    txtName.Clear();
+                }
+
+                UpdateCheckedListBox();
+            }
+            else
+                MessageBox.Show("Please enter a name");            
         }
     }
 }
