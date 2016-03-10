bec4bc4c5f2d82aee167f3640f68fb3babbe33a1
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-11 12:51:12 +1300

    Added rot13 class

---
+++ Weekly practicals/Week 4/EncryptionTool/EncryptionTool/Rot13.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionTool
+{
+    public class Rot13 : IEncryptMachine
+    {
+        public String Encrypt(String str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
