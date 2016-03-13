c54c9f51a504a476c29449d2e19f5768952fc226
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-13 17:38:43 +1300

    Finished TextDisplay methods
    Added code to displayCritterList and clearDisplay

--- Weekly practicals/Week 4/Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
+++ Weekly practicals/Week 4/Animal Shelter Skeleton/IN710 4.1 Animal Shelter Solution 2014/TextDisplay.cs
@@ -8,24 +8,30 @@ namespace IN710_4._1_Animal_Shelter_Solution_2014
 {
     public class TextDisplay: IDisplay
     {
+        //Properties
         private ListBox displayTextListBox;
 
+        //Ctor
         public TextDisplay(ListBox listBox)
         {
             displayTextListBox = listBox;
         }
 
+        //Adds all critters to ListBox from the critterList
         public void displayCritterList(List<Critter> critterList)
         {
             foreach(Critter critter in critterList)
             {
-                displayTextListBox.Items.Add
+                //Adds the critter's species and name
+                String strToAdd = critter.Species + ": " + critter.Name;
+                displayTextListBox.Items.Add(strToAdd);
             }
         }
 
+        //Clears all items from ListBox
         public void clearDisplay()
         {
-            throw new NotImplementedException();
+            displayTextListBox.Items.Clear();
         }
     }
 }
