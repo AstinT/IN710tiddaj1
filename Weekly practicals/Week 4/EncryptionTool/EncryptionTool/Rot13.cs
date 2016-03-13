aa8bd01ab3afedfb3b97fb204001a51320350520
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-13 21:15:43 +1300

    Added Rot13 algorithm

--- Weekly practicals/Week 4/EncryptionTool/EncryptionTool/Rot13.cs
+++ Weekly practicals/Week 4/EncryptionTool/EncryptionTool/Rot13.cs
@@ -10,12 +10,48 @@ namespace EncryptionTool
     {
         public String Encrypt(String stringToEncrypt)
         {
-            throw new NotImplementedException();
+            //Convert string to char array
+            char[]  arrayToEncrypt = stringToEncrypt.ToCharArray();
+            for (int i = 0; i < arrayToEncrypt.Length; i++)
+            {
+                //Casts letter to number
+                int number = (int)arrayToEncrypt[i];
+
+                //lowercase
+                if (number >= 'a' && number <= 'z')
+                {
+                    if (number > 'm')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                //uppercase
+                else if (number >= 'A' && number <= 'Z')
+                {
+                    if (number > 'M')
+                    {
+                        number -= 13;
+                    }
+                    else
+                    {
+                        number += 13;
+                    }
+                }
+                //overwrites char letter to char number in array
+                arrayToEncrypt[i] = (char)number;
+            }
+            //returns char array as a string
+            return new string(arrayToEncrypt);
         }
 
+        //Same code as Encrypt method
         public String Decrypt(String stringToDecrypt)
         {
-            throw new NotImplementedException();
+            return Encrypt(stringToDecrypt);
         }
     }
 }
