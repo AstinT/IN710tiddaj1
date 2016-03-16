a43f61590864a66264c7664ae735d674be087095
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-17 10:15:56 +1300

    Added Bison class

---
+++ Weekly practicals/Week 5/AnimalWorldFactoryPattern/AnimalWorldFactoryPattern/Bison.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorldFactoryPattern
+{
+    public class Bison : Animal
+    {
+        public Bison()
+        {
+            name = "Bison";
+            family = "Herbivore";
+            food = "Grass";
+            image = new Bitmap("bison.jpg");
+        }
+    }
+}
