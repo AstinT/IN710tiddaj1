be3b3b15c40d9a7909f11946a5e02a3a87247016
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-16 16:07:26 +1300

    Added Animal base class

---
+++ Weekly practicals/Week 5/AnimalWorldFactoryPattern/AnimalWorldFactoryPattern/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorldFactoryPattern
+{
+    public class Animal
+    {
+        protected String name;
+        protected String family;
+        protected String food;
+        protected Bitmap image { get; }
+
+        public override String ToString()
+        {
+            String displayString;
+
+            displayString = "I am " + name;
+            displayString += ". I am a " + family;
+            displayString += ". I eat " + food + ".";
+
+            return displayString;
+        }
+    }
+}
