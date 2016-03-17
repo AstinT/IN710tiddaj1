d140d816684fcfd54a03e1085795b9d9ab6600f8
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-17 15:23:59 +1300

    Added continent child NewZealand

---
+++ Weekly practicals/Week 5/AnimalWorldFactoryPattern/AnimalWorldFactoryPattern/NewZealand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalWorldFactoryPattern
+{
+    public class NewZealand : Continent
+    {
+        public NewZealand(ListBox displayBox, Random rGen, int animalTypes, Graphics canvas)
+            : base(displayBox, rGen, animalTypes, canvas)
+        {
+            animalFactory = new NewZealandAnimalFactory();
+        }
+    }
+}
