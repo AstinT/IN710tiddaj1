57749baf2ac15d58e7f3ebe86a8cd891f46b9626
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-17 15:23:24 +1300

    Added NewZealand animals

---
+++ Weekly practicals/Week 5/AnimalWorldFactoryPattern/AnimalWorldFactoryPattern/Kiwi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorldFactoryPattern
+{
+    public class Kiwi : Animal
+    {
+        public Kiwi()
+        {
+            name = "Kiwi";
+            family = "Omnivore";
+            food = "Berries, insects and roots";
+            image = new Bitmap("kiwi.jpg");
+        }
+    }
+}
---
+++ Weekly practicals/Week 5/AnimalWorldFactoryPattern/AnimalWorldFactoryPattern/Sheep.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorldFactoryPattern
+{
+    public class Sheep : Animal
+    {
+        public Sheep()
+        {
+            name = "Sheep";
+            family = "Herbivore";
+            food = "Grass";
+            image = new Bitmap("sheep.jpg");
+        }
+    }
+}
