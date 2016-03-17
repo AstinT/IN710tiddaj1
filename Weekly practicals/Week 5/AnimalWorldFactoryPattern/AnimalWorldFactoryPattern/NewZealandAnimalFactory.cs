8ed453b970cac93cdc29edbe55740a6a6fd350f1
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-17 15:24:58 +1300

    Added NewZealandAnimalFactory class

---
+++ Weekly practicals/Week 5/AnimalWorldFactoryPattern/AnimalWorldFactoryPattern/NewZealandAnimalFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalWorldFactoryPattern
+{
+    public class NewZealandAnimalFactory : IAnimalFactory
+    {
+        public Animal createAnimal(int animalCode)
+        {
+            Animal newAnimal = null;
+
+            switch (animalCode)
+            {
+                case 0:
+                    newAnimal = new Sheep();
+                    break;
+                case 1:
+                    newAnimal = new Kiwi();
+                    break;
+                case 2:
+                    newAnimal = new Kea();
+                    break;
+            }
+            return newAnimal;
+        }
+    }
+}
