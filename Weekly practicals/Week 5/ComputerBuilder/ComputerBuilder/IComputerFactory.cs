be3576d0c1823034388770f811c898727013e88e
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-20 13:43:33 +1300

    Added IComputerFactory

---
+++ Weekly practicals/Week 5/ComputerBuilder/ComputerBuilder/IComputerFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerBuilder
+{
+    public interface IComputerFactory
+    {
+        Cpu buildCpu();
+        Ram buildRam();
+        Gpu buildGpu();
+    }
+}
