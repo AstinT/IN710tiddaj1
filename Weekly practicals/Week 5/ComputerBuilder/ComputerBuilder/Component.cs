c97b7783821ae64731bb07ca1adac96e91012c02
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-20 13:44:46 +1300

    Added Component parent class

---
+++ Weekly practicals/Week 5/ComputerBuilder/ComputerBuilder/Component.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerBuilder
+{
+    public class Component
+    {
+        //Properties
+        protected int price;
+        protected String name;
+
+        public int Price
+        {
+            get {return price;}
+        }
+
+        public override string ToString()
+        {
+            String displayString;
+
+            displayString = price + "\t" + name;
+
+            return displayString;
+        }
+    }
+}
