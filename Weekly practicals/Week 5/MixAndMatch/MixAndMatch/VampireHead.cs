2720f3801b963b497ed353d703c77f43a6a61d08
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-21 18:06:12 +1300

    Added all vampire classes

---
+++ Weekly practicals/Week 5/MixAndMatch/MixAndMatch/VampireHead.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MixAndMatch
+{
+    public class VampireHead : Head
+    {
+        public VampireHead()
+        {
+            image = Image.FromFile("Vampire_0.png");
+        }
+    }
+}
---
+++ Weekly practicals/Week 5/MixAndMatch/MixAndMatch/VampireLegs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MixAndMatch
+{
+    public class VampireLegs : Legs
+    {
+        public VampireLegs()
+        {
+            image = Image.FromFile("Vampire_2.png");
+        }
+    }
+}
