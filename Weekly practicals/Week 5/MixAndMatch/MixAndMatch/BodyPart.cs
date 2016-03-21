71665ec285b9b3d1231b4cec3ccc5d7c97f1d7d6
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-21 18:04:40 +1300

    Added parent class BodyPart

---
+++ Weekly practicals/Week 5/MixAndMatch/MixAndMatch/BodyPart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MixAndMatch
+{
+    public class BodyPart
+    {
+        protected Image image;
+
+        //Read only
+        public Image Image
+        {
+            get { return image; }
+        }
+    }
+}
