36c90aa4f2e6d73b5d21816ee878d042f8efb72f
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-31 19:54:09 +1300

    Finished form code

--- Weekly practicals/Week 6/SocialNetwork/SocialNetwork/Form1.cs
+++ Weekly practicals/Week 6/SocialNetwork/SocialNetwork/Form1.cs
@@ -12,14 +12,37 @@ namespace SocialNetwork
 {
     public partial class Form1 : Form
     {
+        //Properties
+        private StatusMonitor StatusSubject;
+
+        //Constructor
         public Form1()
         {
             InitializeComponent();
+
+            StatusSubject = new StatusMonitor();
+
+            new FriendObserver(lbFriend1, StatusSubject);
+            new FriendObserver(lbFriend2, StatusSubject);
+            new FriendObserver(lbFriend3, StatusSubject);
+            new FriendObserver(lbFriend4, StatusSubject);
         }
 
+        //Click method
         private void btnUpdateStatus_Click(object sender, EventArgs e)
         {
+            if (tbUpdateStatus.Text != "")
+            {
+                StatusSubject.NewStatus = tbUpdateStatus.Text;
+                StatusSubject.NotifyObservers();
 
+                lbMain.Items.Clear();
+                lbMain.Items.Add(tbUpdateStatus.Text);
+                
+                tbUpdateStatus.Text = "";
+            }
+            else
+                MessageBox.Show("Please enter a status.");            
         }
     }
 }
