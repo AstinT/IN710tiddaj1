1ec98187034594b43b0d272cbce2bdc03ba1cbfe
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-31 19:54:31 +1300

    IDE stuff

--- Weekly practicals/Week 6/SocialNetwork/SocialNetwork/Form1.Designer.cs
+++ Weekly practicals/Week 6/SocialNetwork/SocialNetwork/Form1.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tbpMain = new System.Windows.Forms.TabPage();
-            this.tbpFriend1 = new System.Windows.Forms.TabPage();
-            this.tbpFriend2 = new System.Windows.Forms.TabPage();
-            this.tbpFriend3 = new System.Windows.Forms.TabPage();
-            this.tbpFriend4 = new System.Windows.Forms.TabPage();
-            this.lbMain = new System.Windows.Forms.ListBox();
-            this.btnUpdateStatus = new System.Windows.Forms.Button();
             this.tbUpdateStatus = new System.Windows.Forms.TextBox();
+            this.btnUpdateStatus = new System.Windows.Forms.Button();
+            this.lbMain = new System.Windows.Forms.ListBox();
+            this.tbpFriend1 = new System.Windows.Forms.TabPage();
             this.lbFriend1 = new System.Windows.Forms.ListBox();
+            this.tbpFriend2 = new System.Windows.Forms.TabPage();
             this.lbFriend2 = new System.Windows.Forms.ListBox();
+            this.tbpFriend3 = new System.Windows.Forms.TabPage();
             this.lbFriend3 = new System.Windows.Forms.ListBox();
+            this.tbpFriend4 = new System.Windows.Forms.TabPage();
             this.lbFriend4 = new System.Windows.Forms.ListBox();
             this.tabControl1.SuspendLayout();
             this.tbpMain.SuspendLayout();
@@ -75,54 +75,12 @@
             this.tbpMain.Text = "Main Page";
             this.tbpMain.UseVisualStyleBackColor = true;
             // 
-            // tbpFriend1
+            // tbUpdateStatus
             // 
-            this.tbpFriend1.Controls.Add(this.lbFriend1);
-            this.tbpFriend1.Location = new System.Drawing.Point(4, 22);
-            this.tbpFriend1.Name = "tbpFriend1";
-            this.tbpFriend1.Padding = new System.Windows.Forms.Padding(3);
-            this.tbpFriend1.Size = new System.Drawing.Size(631, 395);
-            this.tbpFriend1.TabIndex = 1;
-            this.tbpFriend1.Text = "Friend 1";
-            this.tbpFriend1.UseVisualStyleBackColor = true;
-            // 
-            // tbpFriend2
-            // 
-            this.tbpFriend2.Controls.Add(this.lbFriend2);
-            this.tbpFriend2.Location = new System.Drawing.Point(4, 22);
-            this.tbpFriend2.Name = "tbpFriend2";
-            this.tbpFriend2.Size = new System.Drawing.Size(631, 395);
-            this.tbpFriend2.TabIndex = 2;
-            this.tbpFriend2.Text = "Friend 2";
-            this.tbpFriend2.UseVisualStyleBackColor = true;
-            // 
-            // tbpFriend3
-            // 
-            this.tbpFriend3.Controls.Add(this.lbFriend3);
-            this.tbpFriend3.Location = new System.Drawing.Point(4, 22);
-            this.tbpFriend3.Name = "tbpFriend3";
-            this.tbpFriend3.Size = new System.Drawing.Size(631, 395);
-            this.tbpFriend3.TabIndex = 3;
-            this.tbpFriend3.Text = "Friend 3";
-            this.tbpFriend3.UseVisualStyleBackColor = true;
-            // 
-            // tbpFriend4
-            // 
-            this.tbpFriend4.Controls.Add(this.lbFriend4);
-            this.tbpFriend4.Location = new System.Drawing.Point(4, 22);
-            this.tbpFriend4.Name = "tbpFriend4";
-            this.tbpFriend4.Size = new System.Drawing.Size(631, 395);
-            this.tbpFriend4.TabIndex = 4;
-            this.tbpFriend4.Text = "Friend 4";
-            this.tbpFriend4.UseVisualStyleBackColor = true;
-            // 
-            // lbMain
-            // 
-            this.lbMain.FormattingEnabled = true;
-            this.lbMain.Location = new System.Drawing.Point(7, 27);
-            this.lbMain.Name = "lbMain";
-            this.lbMain.Size = new System.Drawing.Size(618, 290);
-            this.lbMain.TabIndex = 0;
+            this.tbUpdateStatus.Location = new System.Drawing.Point(216, 347);
+            this.tbUpdateStatus.Name = "tbUpdateStatus";
+            this.tbUpdateStatus.Size = new System.Drawing.Size(409, 20);
+            this.tbUpdateStatus.TabIndex = 2;
             // 
             // btnUpdateStatus
             // 
@@ -134,43 +92,85 @@
             this.btnUpdateStatus.UseVisualStyleBackColor = true;
             this.btnUpdateStatus.Click += new System.EventHandler(this.btnUpdateStatus_Click);
             // 
-            // tbUpdateStatus
+            // lbMain
             // 
-            this.tbUpdateStatus.Location = new System.Drawing.Point(216, 347);
-            this.tbUpdateStatus.Name = "tbUpdateStatus";
-            this.tbUpdateStatus.Size = new System.Drawing.Size(409, 20);
-            this.tbUpdateStatus.TabIndex = 2;
+            this.lbMain.FormattingEnabled = true;
+            this.lbMain.Location = new System.Drawing.Point(7, 27);
+            this.lbMain.Name = "lbMain";
+            this.lbMain.Size = new System.Drawing.Size(618, 290);
+            this.lbMain.TabIndex = 0;
+            // 
+            // tbpFriend1
+            // 
+            this.tbpFriend1.Controls.Add(this.lbFriend1);
+            this.tbpFriend1.Location = new System.Drawing.Point(4, 22);
+            this.tbpFriend1.Name = "tbpFriend1";
+            this.tbpFriend1.Padding = new System.Windows.Forms.Padding(3);
+            this.tbpFriend1.Size = new System.Drawing.Size(631, 395);
+            this.tbpFriend1.TabIndex = 1;
+            this.tbpFriend1.Text = "Friend 1";
+            this.tbpFriend1.UseVisualStyleBackColor = true;
             // 
             // lbFriend1
             // 
             this.lbFriend1.FormattingEnabled = true;
-            this.lbFriend1.Location = new System.Drawing.Point(7, 30);
+            this.lbFriend1.Location = new System.Drawing.Point(6, 4);
             this.lbFriend1.Name = "lbFriend1";
-            this.lbFriend1.Size = new System.Drawing.Size(618, 355);
+            this.lbFriend1.Size = new System.Drawing.Size(619, 381);
             this.lbFriend1.TabIndex = 0;
+            // 
+            // tbpFriend2
+            // 
+            this.tbpFriend2.Controls.Add(this.lbFriend2);
+            this.tbpFriend2.Location = new System.Drawing.Point(4, 22);
+            this.tbpFriend2.Name = "tbpFriend2";
+            this.tbpFriend2.Size = new System.Drawing.Size(631, 395);
+            this.tbpFriend2.TabIndex = 2;
+            this.tbpFriend2.Text = "Friend 2";
+            this.tbpFriend2.UseVisualStyleBackColor = true;
             // 
             // lbFriend2
             // 
             this.lbFriend2.FormattingEnabled = true;
-            this.lbFriend2.Location = new System.Drawing.Point(4, 27);
+            this.lbFriend2.Location = new System.Drawing.Point(3, 3);
             this.lbFriend2.Name = "lbFriend2";
-            this.lbFriend2.Size = new System.Drawing.Size(624, 355);
+            this.lbFriend2.Size = new System.Drawing.Size(624, 381);
             this.lbFriend2.TabIndex = 0;
+            // 
+            // tbpFriend3
+            // 
+            this.tbpFriend3.Controls.Add(this.lbFriend3);
+            this.tbpFriend3.Location = new System.Drawing.Point(4, 22);
+            this.tbpFriend3.Name = "tbpFriend3";
+            this.tbpFriend3.Size = new System.Drawing.Size(631, 395);
+            this.tbpFriend3.TabIndex = 3;
+            this.tbpFriend3.Text = "Friend 3";
+            this.tbpFriend3.UseVisualStyleBackColor = true;
             // 
             // lbFriend3
             // 
             this.lbFriend3.FormattingEnabled = true;
-            this.lbFriend3.Location = new System.Drawing.Point(4, 27);
+            this.lbFriend3.Location = new System.Drawing.Point(3, 3);
             this.lbFriend3.Name = "lbFriend3";
-            this.lbFriend3.Size = new System.Drawing.Size(624, 355);
+            this.lbFriend3.Size = new System.Drawing.Size(624, 381);
             this.lbFriend3.TabIndex = 0;
+            // 
+            // tbpFriend4
+            // 
+            this.tbpFriend4.Controls.Add(this.lbFriend4);
+            this.tbpFriend4.Location = new System.Drawing.Point(4, 22);
+            this.tbpFriend4.Name = "tbpFriend4";
+            this.tbpFriend4.Size = new System.Drawing.Size(631, 395);
+            this.tbpFriend4.TabIndex = 4;
+            this.tbpFriend4.Text = "Friend 4";
+            this.tbpFriend4.UseVisualStyleBackColor = true;
             // 
             // lbFriend4
             // 
             this.lbFriend4.FormattingEnabled = true;
-            this.lbFriend4.Location = new System.Drawing.Point(4, 19);
+            this.lbFriend4.Location = new System.Drawing.Point(4, 6);
             this.lbFriend4.Name = "lbFriend4";
-            this.lbFriend4.Size = new System.Drawing.Size(624, 368);
+            this.lbFriend4.Size = new System.Drawing.Size(624, 381);
             this.lbFriend4.TabIndex = 0;
             // 
             // Form1
