107918dcd6c421ede5d0462fec8d49b089148d6b
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-31 19:54:00 +1300

    Finished observer code

---
+++ Weekly practicals/Week 6/SocialNetwork/SocialNetwork/FriendObserver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocialNetwork
+{
+    public class FriendObserver : IObserver
+    {
+        //Properties
+        private String newStatus;
+        private ListBox displayBox;
+        private StatusMonitor statusSubject;
+        private DateTime timeStamp;
+
+        //Constructor
+        public FriendObserver(ListBox displayBox, StatusMonitor statusSubject)
+        {
+            this.displayBox = displayBox;
+            this.statusSubject = statusSubject;
+
+            newStatus = "";
+
+            statusSubject.AddObserver(this);
+        }
+
+        public void Update(String newStatus)
+        {
+            this.newStatus = newStatus;
+            timeStamp = DateTime.Now;
+        }
+
+        public void Display()
+        {
+            displayBox.Items.Add(timeStamp);
+            displayBox.Items.Add(newStatus);
+            displayBox.Items.Add("------------------------------");
+        }        
+    }
+}
---
+++ Weekly practicals/Week 6/SocialNetwork/SocialNetwork/IObserver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork
+{
+    public interface IObserver
+    {
+        void Update(String newStatus);
+        void Display();
+    }
+}
