2f4ea3c1a3e4b0065a9389381bf137268609dc4f
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-31 19:53:37 +1300

    Finished Subject code

---
+++ Weekly practicals/Week 6/SocialNetwork/SocialNetwork/StatusMonitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SocialNetwork
+{
+    public class StatusMonitor : ISubject
+    {
+        //Properties
+        private List<IObserver> observerList;
+        private String newStatus;
+
+        //Constructor
+        public StatusMonitor()
+        {
+            observerList = new List<IObserver>();
+            newStatus = "";
+        }
+
+        //Getter and setter
+        public String NewStatus
+        {
+            get { return newStatus; }
+            set { newStatus = value; }
+        }
+
+        //Methods
+        public void AddObserver(IObserver o)
+        {
+            observerList.Add(o);
+        }
+
+        public void RemoveObserver(IObserver o)
+        {
+            observerList.Remove(o);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach(IObserver currObserver in observerList)
+            {
+                currObserver.Update(newStatus);
+                currObserver.Display();
+            }
+        }
+    }
+}
