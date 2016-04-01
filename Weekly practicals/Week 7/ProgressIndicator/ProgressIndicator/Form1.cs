1e91a426d764b100c042ee1e3e18113769443c75
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-01 16:46:34 +1300

    Worked on Delegate practical one

---
+++ Weekly practicals/Week 7/ProgressIndicator/ProgressIndicator/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgressIndicator
+{
+    public partial class Form1 : Form
+    {
+        public delegate void FeedbackDelegate();
+        FeedbackDelegate feedbackDelegate;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            feedbackDelegate = null;
+        }
+
+        public void slowMethod(FeedbackDelegate operation)
+        {
+            for (int i = 0; i < 10; i++)
+            {                
+                operation();
+                Thread.Sleep(500);
+                Application.DoEvents();
+            }            
+        }
+
+        public void updateNumericUpDown()
+        {
+            numericUpDown1.Value++;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(rbNumericUpDown.Checked)
+            {
+                feedbackDelegate = updateNumericUpDown;
+            }
+
+            slowMethod(feedbackDelegate);
+        }
+    }
+}
