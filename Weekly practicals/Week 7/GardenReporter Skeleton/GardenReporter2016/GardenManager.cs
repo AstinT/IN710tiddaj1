490efa1d92e5a5817c37eb7038cef0b68ad83cba
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-04 16:38:09 +1200

    Added GardenManager class

---
+++ Weekly practicals/Week 7/GardenReporter Skeleton/GardenReporter2016/GardenManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GardenReporter2016
+{
+    public class GardenManager
+    {
+        //Delegate
+        public delegate String GardenDelegate(Garden garden);
+        GardenDelegate gardenDelegate;
+
+        //Properties
+        List<Garden> gardenList;
+        ListBox displayBox;
+
+        //Constructor
+        public GardenManager(ListBox displayBox)
+        {
+            gardenList = new List<Garden>();
+            this.displayBox = displayBox;
+            gardenDelegate = null;
+        }
+
+        //Methods
+        public void AddGarden(Garden newGarden)
+        {
+            gardenList.Add(newGarden);
+        }
+
+        public void ProcessGardens()
+        {
+            foreach(Garden currGarden in gardenList)
+            {
+
+            }
+        }
+
+        public String CalculateGardenArea(Garden garden)
+        {
+
+        }
+
+        public String CalculateGardenCharges(Garden garden)
+        {
+
+        }
+    }
+}
