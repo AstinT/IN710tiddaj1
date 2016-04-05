987be216701f85dbf72b9bf6f4c5f8d5c096cdae
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-05 18:55:21 +1200

    Added BeepObserver class

--- Weekly practicals/Week 8/FireAlarm/FireAlarm/BeepObserver.cs
+++ Weekly practicals/Week 8/FireAlarm/FireAlarm/BeepObserver.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FireAlarm
 {
-    class BeepObserver
+    public class BeepObserver : FireAlarmObserverBase
     {
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int freq, int duration);
+
+        public BeepObserver(FireAlarmSubject subject) : base(subject)
+        {
+            //nothing here
+        }
+
+        public override void AlarmMethod(object fireSubject, FireAlarmEventArgs args)
+        {
+            switch (args.FireCategory)
+            {
+                case EFireCategory.MINOR:
+                    Beep(300, 2000);
+                    break;
+                case EFireCategory.SERIOUS:
+                    Beep(600, 2000);
+                    break;
+                case EFireCategory.INFERNO:
+                    Beep(900, 2000);
+                    break;
+            }
+        }
     }
 }
