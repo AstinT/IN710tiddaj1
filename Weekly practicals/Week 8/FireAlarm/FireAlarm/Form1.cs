a9a8759dab269a5cf846784f4e147c786edd6128
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-05 19:26:59 +1200

    Added Form1 code

--- Weekly practicals/Week 8/FireAlarm/FireAlarm/Form1.cs
+++ Weekly practicals/Week 8/FireAlarm/FireAlarm/Form1.cs
@@ -12,9 +12,42 @@ namespace FireAlarm
 {
     public partial class Form1 : Form
     {
+        FireAlarmSubject subject;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            subject = new FireAlarmSubject();
+
+            new InstructionsObserver(subject);
+            new BeepObserver(subject);
+        }
+
+        private void btnFireAlarm_Click(object sender, EventArgs e)
+        {
+            EFireCategory currFireCategory = EFireCategory.NONE;
+
+            if (rbMinor.Checked)
+            {
+                currFireCategory = EFireCategory.MINOR;
+            }
+            else if (rbSerious.Checked)
+            {
+                currFireCategory = EFireCategory.SERIOUS;
+            }
+            else if (rbInferno.Checked)
+            {
+                currFireCategory = EFireCategory.INFERNO;
+            }
+            else
+                MessageBox.Show("Please select a radio button.");
+
+            if (currFireCategory != EFireCategory.NONE)
+                subject.OnFireEvent(currFireCategory);
+        }        
     }
 }
