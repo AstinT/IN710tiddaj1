02d2007b6750236d794fbd4ff09a9465ce95e77f
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-05 18:32:30 +1200

    Added FireAlarmSubject class

--- Weekly practicals/Week 8/FireAlarm/FireAlarm/FireAlarmSubject.cs
+++ Weekly practicals/Week 8/FireAlarm/FireAlarm/FireAlarmSubject.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace FireAlarm
 {
-    class FireAlarmSubject
+    public class FireAlarmSubject
     {
+        //Delegate
+        //Passed a custom EventArgs called FireAlarmEventArgs
+        public delegate void FireEventHandler(object fireSubject, FireAlarmEventArgs args);
+        
+        //Event
+        public event FireEventHandler FireEvent;
+
+        public void OnFireEvent(EFireCategory fCat)
+        {
+            FireAlarmEventArgs args = new FireAlarmEventArgs(fCat);
+
+            if (FireEvent != null)
+                FireEvent(this, args);
+        }
     }
 }
