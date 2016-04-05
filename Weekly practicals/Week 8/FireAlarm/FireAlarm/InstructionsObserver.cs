00d4b1bc901cdcab137d1c8dfacc600d41a5bce7
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-05 18:55:34 +1200

    Added InstructionsObserver class

--- Weekly practicals/Week 8/FireAlarm/FireAlarm/InstructionsObserver.cs
+++ Weekly practicals/Week 8/FireAlarm/FireAlarm/InstructionsObserver.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace FireAlarm
 {
-    class InstructionsObserver
+    public class InstructionsObserver : FireAlarmObserverBase
     {
+        public InstructionsObserver(FireAlarmSubject subject) : base(subject)
+        {
+            //nothing here
+        }
+
+        public override void AlarmMethod(object fireSubject, FireAlarmEventArgs args)
+        {
+            String message = "Fire is " + args.FireCategory.ToString() + ".";
+            switch (args.FireCategory)
+            {
+                case EFireCategory.MINOR:
+                    message += " Use a fire extinguisher.";
+                    break;
+                case EFireCategory.SERIOUS:
+                    message += " Call the Fire Department.";
+                    break;
+                case EFireCategory.INFERNO:
+                    message += " Run for your life!!!!!.";
+                    break;
+            }
+
+            MessageBox.Show(message);
+        }
     }
 }
