c679dcf08bfc5285231eff33da253de1dc0e1a30
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-17 17:59:55 +1200

    Worked on xml prac

---
+++ Weekly practicals/Week 9/XMLDunedinGigs/XMLDunedinGigs/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace XMLDunedinGigs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.colBandName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colGenre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVenue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDateTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAllUpcomingEvents = new System.Windows.Forms.Button();
+            this.btnHardRock = new System.Windows.Forms.Button();
+            this.btnCurrentMonth = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colBandName,
+            this.colGenre,
+            this.colVenue,
+            this.colDateTime});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(567, 387);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // colBandName
+            // 
+            this.colBandName.HeaderText = "Band Name";
+            this.colBandName.Name = "colBandName";
+            // 
+            // colGenre
+            // 
+            this.colGenre.HeaderText = "Genre";
+            this.colGenre.Name = "colGenre";
+            // 
+            // colVenue
+            // 
+            this.colVenue.HeaderText = "Venue";
+            this.colVenue.Name = "colVenue";
+            // 
+            // colDateTime
+            // 
+            this.colDateTime.HeaderText = "Date and Time";
+            this.colDateTime.Name = "colDateTime";
+            // 
+            // btnAllUpcomingEvents
+            // 
+            this.btnAllUpcomingEvents.Location = new System.Drawing.Point(13, 35);
+            this.btnAllUpcomingEvents.Name = "btnAllUpcomingEvents";
+            this.btnAllUpcomingEvents.Size = new System.Drawing.Size(157, 23);
+            this.btnAllUpcomingEvents.TabIndex = 1;
+            this.btnAllUpcomingEvents.Text = "All Upcoming Events";
+            this.btnAllUpcomingEvents.UseVisualStyleBackColor = true;
+            this.btnAllUpcomingEvents.Click += new System.EventHandler(this.btnAllUpcomingEvents_Click);
+            // 
+            // btnHardRock
+            // 
+            this.btnHardRock.Location = new System.Drawing.Point(218, 34);
+            this.btnHardRock.Name = "btnHardRock";
+            this.btnHardRock.Size = new System.Drawing.Size(157, 23);
+            this.btnHardRock.TabIndex = 2;
+            this.btnHardRock.Text = "Hard Rock";
+            this.btnHardRock.UseVisualStyleBackColor = true;
+            this.btnHardRock.Click += new System.EventHandler(this.btnHardRock_Click);
+            // 
+            // btnCurrentMonth
+            // 
+            this.btnCurrentMonth.Location = new System.Drawing.Point(423, 34);
+            this.btnCurrentMonth.Name = "btnCurrentMonth";
+            this.btnCurrentMonth.Size = new System.Drawing.Size(157, 23);
+            this.btnCurrentMonth.TabIndex = 3;
+            this.btnCurrentMonth.Text = "Current Month";
+            this.btnCurrentMonth.UseVisualStyleBackColor = true;
+            this.btnCurrentMonth.Click += new System.EventHandler(this.btnCurrentMonth_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 498);
+            this.Controls.Add(this.btnCurrentMonth);
+            this.Controls.Add(this.btnHardRock);
+            this.Controls.Add(this.btnAllUpcomingEvents);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colBandName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colGenre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colVenue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDateTime;
+        private System.Windows.Forms.Button btnAllUpcomingEvents;
+        private System.Windows.Forms.Button btnHardRock;
+        private System.Windows.Forms.Button btnCurrentMonth;
+    }
+}
+
---
+++ Weekly practicals/Week 9/XMLDunedinGigs/XMLDunedinGigs/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace XMLDunedinGigs
+{
+    public partial class Form1 : Form
+    {
+        XDocument pubsAndClubsXML;
+        DataGridViewRowCollection dgvRows;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pubsAndClubsXML = XDocument.Load("pubsAndClubs.xml");
+            dgvRows = dataGridView1.Rows;
+        }
+
+        public void loadAllUpcomingEvents()
+        {
+            dataGridView1.Rows.Clear();            
+
+            foreach (XElement gig in pubsAndClubsXML.Element("Event_Guide").Elements("Gig"))
+            {
+                String bandName = gig.Element("Band").Element("Name").Value;
+                String genre = gig.Element("Band").Element("Genre").Value;
+                String venue = gig.Element("Venue").Value;
+                String day = gig.Element("Date").Attribute("day").Value;
+                String month = gig.Element("Date").Attribute("month").Value;
+                String year = gig.Element("Date").Attribute("year").Value;
+                String time = gig.Element("Time").Value;
+
+                String date = day + "/" + month + "/" + year;
+                String dateTime = date + "  " + time;
+
+                String[] newRow = { bandName, genre, venue, dateTime };
+
+                dgvRows.Add(newRow);
+            }
+        }
+
+        public void loadAllHardRockEvents()
+        {
+            dataGridView1.Rows.Clear();            
+
+            foreach (XElement gig in pubsAndClubsXML.Element("Event_Guide").Elements("Gig"))
+            {
+                if(gig.Element("Band").Element("Genre").Value.Trim().Equals("Hard Rock"))
+                {
+                    String bandName = gig.Element("Band").Element("Name").Value;
+                    String genre = gig.Element("Band").Element("Genre").Value;
+                    String venue = gig.Element("Venue").Value;
+                    String day = gig.Element("Date").Attribute("day").Value;
+                    String month = gig.Element("Date").Attribute("month").Value;
+                    String year = gig.Element("Date").Attribute("year").Value;
+                    String time = gig.Element("Time").Value;
+
+                    String date = day + "/" + month + "/" + year;
+                    String dateTime = date + "  " + time;
+
+                    String[] newRow = { bandName, genre, venue, dateTime };
+
+                    dgvRows.Add(newRow);
+                }
+            }
+        }
+
+        private void btnAllUpcomingEvents_Click(object sender, EventArgs e)
+        {
+            loadAllUpcomingEvents();
+        }
+
+        private void btnHardRock_Click(object sender, EventArgs e)
+        {
+            loadAllHardRockEvents();
+        }
+
+        private void btnCurrentMonth_Click(object sender, EventArgs e)
+        {
+
+        }        
+    }
+}
