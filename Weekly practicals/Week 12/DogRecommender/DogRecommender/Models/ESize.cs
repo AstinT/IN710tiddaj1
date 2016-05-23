15f5caf4fbaf33b69e9cdb2999dfc39f4adc5cdd
Astin Tiddy <Astin8@hotmail.co.nz>
2016-05-23 23:03:28 +1200

    Worked on practical

---
+++ Weekly practicals/Week 12/DogRecommender/DogRecommender/Models/ESize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DogRecommender.Models
+{
+    public enum ESize
+    {
+        None,
+        Miniature,
+        Small,
+        Medium,
+        Large,
+        Giant
+    }
+}
